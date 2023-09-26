19b8bfb8c08f1ea477c9d2f3519169fe710d4810
MrMilanC <69223405+MrMilanC@users.noreply.github.com>
2023-09-26 21:10:09 +0200

    .yml file updated

--- src/test/java/com/example/wiesenbluetewebshop/WiesenblueteWebshopApplicationTests.java
+++ src/test/java/com/example/wiesenbluetewebshop/WiesenblueteWebshopApplicationTests.java
@@ -2,12 +2,16 @@ package com.example.wiesenbluetewebshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class WiesenblueteWebshopApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
