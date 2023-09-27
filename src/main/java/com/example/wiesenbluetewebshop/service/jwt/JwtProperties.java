6113c464065cf4a45942cfaf04dcf53f3edc41b3
MrMilanC <69223405+MrMilanC@users.noreply.github.com>
2023-09-27 11:27:19 +0200

    JWT Section added

---
+++ src/main/java/com/example/wiesenbluetewebshop/service/jwt/JwtProperties.java
@@ -0,0 +1,18 @@
+package com.example.wiesenbluetewebshop.service.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties("security.jwt")
+public class JwtProperties {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+}
